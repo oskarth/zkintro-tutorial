d3e653635592298a72368161b4aead7aae42310d
oskarth <oskarth@titanproxy.com>
2024-06-19 12:40:10 +0800

    fix: Update build script to take arg

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
-echo "Building example.circom circuit..."
-mkdir -p example/target
-circom example/example.circom --r1cs --wasm --output example/target
+# This script builds a circuit from a specified Circom file.
+# It requires a single argument: the path to the Circom file.
+# The script outputs the compiled circuit files into a 'target' directory located in the same directory as the Circom file.
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <path_to_circom_file>"
+    exit 1
+fi
+
+CIRCOM_FILE=$1
+TARGET_DIR=$(dirname "$CIRCOM_FILE")/target
+
+echo "Building $CIRCOM_FILE circuit..."
+mkdir -p "$TARGET_DIR"
+circom "$CIRCOM_FILE" --r1cs --wasm --output "$TARGET_DIR"
